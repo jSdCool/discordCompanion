279611b3ba18d5461b7cd43f6e0d0100cfe6255d
jSdCool <37940266+jSdCool@users.noreply.github.com>
2022-08-05 19:08:25 -0400

    update to 1.19.2 fix /say not being detected

--- src/main/java/com/discordcomp/mixins/Mixin_ReadChat.java
+++ src/main/java/com/discordcomp/mixins/Mixin_ReadChat.java
@@ -4,6 +4,7 @@ import com.discordcomp.Main;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,7 +19,7 @@ public class
 Mixin_ReadChat {
 
    @Inject(method = "broadcast(Lnet/minecraft/text/Text;Z)V",at =@At("HEAD"),remap = true )
-    private void broadcastChatMessage(Text text, boolean overlay, CallbackInfo ci){
+    private void broadcastChatMessage(Text text, boolean overlay, CallbackInfo ci){//detect when a system message is sent
 
            if (text.getString().length() > 9 && text.getString().startsWith("Discord ["))
                return;
@@ -31,10 +32,16 @@ Mixin_ReadChat {
     //PlayerManager#broadcastChatMessage    inject here
 
     @Inject(method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/network/message/MessageType$Parameters;)V",at=@At("HEAD"),remap = true)
-    private void broadcastChatMessage(SignedMessage message, ServerPlayerEntity sender, MessageType.Parameters params, CallbackInfo ci){
+    private void broadcastChatMessage(SignedMessage message, ServerPlayerEntity sender, MessageType.Parameters params, CallbackInfo ci){//detetct when a player sends a message
 
         String msg=message.getContent().getString();
         Main.sendMessage("<"+sender.getName().getString()+"> "+message.getContent().getString());
+    }
+
+    @Inject(method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/network/message/MessageType$Parameters;)V",at=@At("HEAD"),remap = true)
+    private void broadcastChatMessage(SignedMessage message, ServerCommandSource source, MessageType.Parameters params, CallbackInfo ci){//detect when the /say command is used
+        String msg=message.getContent().getString();
+        Main.sendMessage("["+source.getName()+"] "+msg);
     }
 
 }
